bf74b3ac8fa34d77fa381469ac8cfd3237f5d647
mihailignatenko <michael@collabimo.com>
2015-05-22 17:30:44 +0300

    changed admin site title

--- forecast/admin.py
+++ forecast/admin.py
@@ -14,6 +14,7 @@ class CustomUserProfileInline(admin.StackedInline):
 class UserAdmin(UserAdmin):
     inlines = (CustomUserProfileInline,)
 
+admin.site.site_header = 'Peleus administration'
 
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
