af6224f7a00d0170b6a176ea16bda99230f67b10
mihailignatenko <michael@collabimo.com>
2015-05-22 15:58:13 +0300

    added CustomUserProfile inline edit

--- forecast/admin.py
+++ forecast/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
 import models
 
 
-admin.site.register(models.CustomUserProfile)
+class CustomUserProfileInline(admin.StackedInline):
+    model = models.CustomUserProfile
+    verbose_name = 'forecast user'
+    verbose_name_plural = 'forecast users'
+
+
+class UserAdmin(UserAdmin):
+    inlines = (CustomUserProfileInline,)
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
 admin.site.register(models.Forecast)
 admin.site.register(models.ForecastVotes)
