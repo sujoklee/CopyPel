0df763ba9a211daa157330e97dee8bf74346ceac
Albert Lee <albertlee@yandex.ru>
2015-05-25 17:37:02 +0300

    profile_page added

--- Peleus/urls.py
+++ Peleus/urls.py
@@ -14,5 +14,6 @@ urlpatterns = patterns('',
                        url(r'signup_last/$', views.SignUpSecondView.as_view(), name='signup2'),
                        url(r'login/$', views.LoginView.as_view(), name='login'),
                        url(r'logout/$', views.LogoutView.as_view(), name='logout'),
+                       url(r'profile/$', TemplateView.as_view(template_name='profile_page.html'), name='profile'),
 
                        )
