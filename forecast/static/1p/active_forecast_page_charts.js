3f3013fe3d2472f05738dc98e65a7b275992809e
vwvolodya <vwvolodya@gmail.com>
2015-06-03 14:47:04 +0300

    slight change to active forecasts

--- forecast/static/1p/active_forecast_page_charts.js
+++ forecast/static/1p/active_forecast_page_charts.js
@@ -1,11 +1,12 @@
 $(document).ready(function (e) {
+     console.log('hello');
     $.ajax({
         url: "/forecasts/",
         method: 'GET'
 
     }).done(function (data) {
         for (var i=0; i<data.length; i++){
-            draw_timeseries_chart('#peleus-forecast-'+data[i].id, data[i].votes)
+            draw_timeseries_chart('#peleus-forecast-'+data[i].id, data[i].votes);
         }
     });
 });
