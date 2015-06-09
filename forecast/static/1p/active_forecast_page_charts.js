9498d5fea27ef82beaee8753a7528d1f34069dd8
mihailignatenko <michael@collabimo.com>
2015-06-09 17:06:03 +0300

    added related media to the forecasts page and fixed chart in active

--- forecast/static/1p/active_forecast_page_charts.js
+++ forecast/static/1p/active_forecast_page_charts.js
@@ -5,8 +5,14 @@ $(document).ready(function (e) {
         method: 'GET'
 
     }).done(function (data) {
-        for (var i=0; i<data.length; i++){
-            draw_timeseries_chart('#peleus-forecast-'+data[i].id, data[i].votes);
+        for (var i=0; i<data.length; i++) {
+            (function (i) {
+                $('#collapseOne-' + data[i].id).on('shown.bs.collapse', function () {
+                    draw_timeseries_chart('#peleus-forecast-' + data[i].id, data[i].votes);
+                }).on('hidden.bs.collapse', function () {
+                    $('#peleus-forecast-' + data[i].id).empty();
+                });
+            })(i);
         }
     });
 });
