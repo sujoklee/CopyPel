43af81268fd67b18ec72ccc73e34896cd916cfcc
Albert Lee <albertlee@yandex.ru>
2015-06-17 18:18:24 +0300

    working on posts showing

--- forecast/static/1p/profile.js
+++ forecast/static/1p/profile.js
@@ -10,7 +10,26 @@ $().ready(function() {
                 console.log(data);
             });
 
+        //$('#SendMessage').on('shown.bs.modal', function() {
+        //    $.ajax({
+        //        url: "/profile_forecasts/{{ profile.id }}/",
+        //        method: 'GET'
+        //
+        //    }).done(function(data) {
+        //        $('#message-set-modal').html(data);
+        //        $.ajax({
+        //            url: "/messages/write/",
+        //            method: 'GET'
+        //        }).on('hidden.bs.modal', function() {
+        //          $('#message-set-modal').empty()
+        //        });
+        });
+
+
+
 
     });
-    });
+
+
+
 
